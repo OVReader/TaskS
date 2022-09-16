27f656000b40837be6e467b26e5c29bb4b9d36e6
LAPTOP-S1RS66RO\marcu <marcusang1999@live.com>
2022-09-16 17:00:37 +0800

    Add explainatory comments

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -15,6 +15,7 @@ public class Main {
         DataManager dm = new DataManager("./data/data.txt");
         ArrayList<Task> tasksData = dm.loadData();
 
+//        System.out.println("All data:");
         System.out.println("Printing deadlines");
         printDeadlines(tasksData);
 
